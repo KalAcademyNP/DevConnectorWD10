9464f96884c10255c7fb805e82fc8f38e294b8f8
Kal <info@kalacademy.org>
2021-07-18 11:04:04 -0700

    generate token

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const bcrypt = require('bcryptjs');
 const gravatar = require('gravatar');
+const jwt = require('jsonwebtoken');
 const router = express.Router();
 const User = require('../../models/User');
+const keys = require('../../config/keys');
 
 //@route   POST  api/users/register
 //@desc    Register a user
@@ -41,5 +43,42 @@ router.post('/register', (req, res) => {
     })
     .catch(err => console.log(err));
 });
+
+//@route   POST  api/users/login
+//@desc    Login a user and generate a token
+//@access  Public 
+router.post('/login', (req,res)=>{
+  User.findOne({email: req.body.email})
+    .then(user => {
+      if (!user){
+        return res.status(404).json({email: 'User not found'});
+      }
+
+      bcrypt.compare(req.body.password, user.password)
+        .then(isMatch => {
+          if (isMatch){
+            //Payload
+            const payload = {
+              id: user.id,
+              name: user.name,
+              avatar: user.avatar
+            };
+
+            //sign token
+            jwt.sign(
+              payload, 
+              keys.secretOrKey,
+              {expiresIn: 3600},
+              (err, token) => {
+                return res.json({token: 'Bearer ' + token});
+              })
+          } else {
+            return res.status(400).json({password: 'Incorrect password'});
+          }
+        })
+    })
+    .catch(err => console.log(err))
+});
+
 
 module.exports = router;
