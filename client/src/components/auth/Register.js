26533bfcb0d90cd330c6689c4c23e9a1e0942994
Kal <info@kalacademy.org>
2021-08-14 11:07:14 -0700

    authreducer and action

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
+import {connect} from 'react-redux';
 import axios from 'axios';
 import classnames from 'classnames';
+import {registerUser} from '../../actions/authActions';
 
 class Register extends Component {
   constructor(){
@@ -28,10 +30,11 @@ class Register extends Component {
       password2: this.state.password2
     };
 
-    axios
-      .post('/api/users/register', newUser)
-      .then(res => console.log(res.data))
-      .catch(err => this.setState({errors: err.response.data}));
+    this.props.registerUser(newUser);
+    // axios
+    //   .post('/api/users/register', newUser)
+    //   .then(res => console.log(res.data))
+    //   .catch(err => this.setState({errors: err.response.data}));
   }
 
   render() {
@@ -102,4 +105,4 @@ class Register extends Component {
   }
 }
 
-export default Register;
+export default connect(null, {registerUser})(Register);
