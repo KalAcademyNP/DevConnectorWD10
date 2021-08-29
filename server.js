29dd1c8810b71ab7169c987942cacd12a6bb1dbb
Kal <info@kalacademy.org>
2021-08-29 10:43:59 -0700

    Prepared for deploy

--- server.js
+++ server.js
@@ -22,15 +22,19 @@ mongoose
 app.use(passport.initialize());
 require('./config/passport')(passport);
 
-//Let's write our first route
-app.get('/', (req, res) => res.send('Hello World!'));
-
 //use routes
 app.use('/api/users', users);
 app.use('/api/profile', profile);
 app.use('/api/posts', posts);
 
-const port = 5000;
+if (process.env.NODE_ENV === 'production'){
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+}
+
+const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
 
 
